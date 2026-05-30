41d3432b63ec0562e024f6bd167894e847f38399
Danylo Morhun <danymorhun@gmail.com>
2026-05-30 17:38:59 +0200

    feat(midas): replace skeleton loading with PageLoader

--- apps/web/src/app/(apps)/midas/loading.tsx
+++ apps/web/src/app/(apps)/midas/loading.tsx
@@ -1,15 +1,5 @@
-import { Skeleton } from "@ethos/ui";
+import { PageLoader } from "@/components/PageLoader";
 
 export default function MidasLoading() {
-	return (
-		<div className="flex flex-col gap-6 p-4 pb-28 md:p-6 md:pb-6">
-			<div className="grid grid-cols-2 gap-3 md:grid-cols-4">
-				{Array.from({ length: 4 }).map((_, i) => (
-					<Skeleton key={i} className="h-24 rounded-xl" />
-				))}
-			</div>
-			<Skeleton className="h-64 rounded-xl" />
-			<Skeleton className="h-48 rounded-xl" />
-		</div>
-	);
+	return <PageLoader />;
 }
